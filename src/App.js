a064669bddd9476fc2a7c4268c28ff7d110d307a
Satvik Karanam <ksvsatvik@gmail.com>
2022-09-16 13:34:17 +0530

    "select Tag" and initial selection scenario in Form (react)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import React  from 'react';
+import React from 'react';
 import './App.css';
 import ControlledFormComponent from './components/ControlledFormComponent';
 import EventsFormScenario from './components/EventsFormScenario';
 import EventsThisScenario from './components/EventsThisScenario';
 import PassingArgumentsInEvents from './components/PassingArgumentsInEvents';
+import SelectTagScenario from './components/SelectTagScenario';
 import TextAreaScenario from './components/TextAreaScenario';
 /* import TimerRcc from './components/TimerRcc';
 import TimerRfc from './components/TimerRfc'; */
 
 
 function App() {
-  
+
   return (
     <div>
-       {/*  <EventsThisScenario /> */}
-       {/*  <EventsFormScenario /> */}
-       {/* <PassingArgumentsInEvents /> */}
-       {/* <ControlledFormComponent /> */}
-       <TextAreaScenario />
+      {/*  <EventsThisScenario /> */}
+      {/*  <EventsFormScenario /> */}
+      {/* <PassingArgumentsInEvents /> */}
+      {/* <ControlledFormComponent /> */}
+      {/*   <TextAreaScenario /> */}
+      <SelectTagScenario />
 
     </div>
   );
